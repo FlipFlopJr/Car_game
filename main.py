efce0cc7946607394bf5439d73bafd37291c3305
Andrey Lavrinovich <lavrinovitch.andreas@yandex.ru>
2023-09-29 12:41:34 +0300

    Add car

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+from pygame.sprite import AbstractGroup
 
 
 
@@ -13,6 +14,17 @@ pg.display.set_caption('Car Game')
 
 background = pg.transform.scale(pg.image.load('background.png'),(WIDTH,HEIGHT))
 
+class Car(pg.sprite.Sprite):
+    def __init__(self, x, y, size, filename):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.transform.scale(pg.image.load(filename), (size-20, size))
+        self.image.set_colorkey(self.image.get_at((0,0)))
+        self.rect = self.image.get_rect(center=(x, y))
+
+car = Car(200,250,60,'car2.png')  
+
+
+
 running = True
 while running:
     for event in pg.event.get():
@@ -21,6 +33,7 @@ while running:
             exit()
     
     screen.blit(background, (0,0))
+    screen.blit(car.image, car.rect)
 
 
     pg.display.update()
